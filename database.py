4ed88f1fe1c9e4b55bee147dc9d9585f49df45ab
Mbwika-07 <allanmbwiks@gmail.com>
2024-11-30 19:32:15 +0300

    .

--- database.py
+++ database.py
@@ -61,6 +61,11 @@ def find_all_admins(connection):
 
 
 
+
+
+
+
+
 def find_user_by_name(connection, name):
     with connection:
         return connection.execute(get_user_by_name, (name)).fetchall()
